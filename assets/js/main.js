eceb3ae50fda29cd0b957df2e2dc9ef6fca9f246
unknown <jtam360@gmail.com>
2018-10-09 13:57:46 -0700

    Added clientside login authentication. Added basic logout functionality.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,9 @@
-// Load header
-$(function(){
-  $("#header").load("assets/header.html");
+// Load adminHeader if user is logged in or userHeader if not logged in
+$(function() {
+  if (document.cookie.indexOf("DatabaseSession=Valid") != -1) {
+    $("#header").load("assets/adminHeader.html");
+  }
+  else {
+    $("#header").load("assets/userHeader.html");
+  }
 });
